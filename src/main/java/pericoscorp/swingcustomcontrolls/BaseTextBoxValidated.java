06e579375f60342a26562e84c39fc1d6878ce967
Arturo Salinas <arturo.salinas@stepstone.com>
2015-12-03 10:08:45 -0600

    .gitignore is now working

--- src/main/java/pericoscorp/swingcustomcontrolls/BaseTextBoxValidated.java
+++ src/main/java/pericoscorp/swingcustomcontrolls/BaseTextBoxValidated.java
@@ -53,10 +53,8 @@ public class BaseTextBoxValidated  extends JTextField{
             public void focusLost(FocusEvent e) {                
                 if(getText().length() > getLength()) 
                 { 
-                    JOptionPane.showMessageDialog(getParent(), "Formato invalido!");                
-                    requestFocus();
-                }
-                 
+                    setText(getText().substring(0, getLength()));
+                }                 
                 if(!Regex.isEmpty() &&  !getText().isEmpty() && !getText().matches(Regex) )
                 {
                     JOptionPane.showMessageDialog(getParent(), "Formato invalido!");                
